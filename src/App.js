d2a9e2f4be7d2c122023737667b26b4129eb5b01
WilliamWarmling <willian_warmling@estudante.sesisenai.org.br>
2023-10-12 19:01:20 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import axios from 'axios';
+
+function App() {
+
+  const userConfig = {
+    username: "PedrooNietoo",
+    password: "Pedro2711!"
+  }
+  axios.post('https://poc-intelbras.vercel.app/users/createUser', userConfig)
+    .then(res => {
+      alert(res.status)
+      console.log(res)
+    })
+
+    .catch(error => {
+      alert(error.status)
+      console.log(error)
+    })
+
+  return (
+    <div className="App">
+      <p>{ }</p>
+    </div>
+  );
+}
+
+export default App;
